0a06ab144b4720e7c54f41aadca924b7a4ab7e57
Vladyslav Zhulavskyi <vladyslavzhulavskyi@MacBook-Air-Vladyslav.local>
2018-11-28 14:08:35 +0200

    Create project

---
+++ Bet Helper/Bet Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bet Helper
+//
+//  Created by Vladyslav Zhulavskyi on 11/28/18.
+//  Copyright © 2018 Vladyslav Zhulavskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
